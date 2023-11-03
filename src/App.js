18ceed032e365aac8c1e981ffc61cd1a8c7518db
Rishab-Joshi16 <rish3116@gmail.com>
2023-11-03 09:50:31 +0530

    Lists Demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Sample from './components/Sample';
+import Sample2 from './components/Sample2';
+import Lists from './components/Lists';
+import Condition from './components/Condition';
+import Events from './components/Events';
+import ClassComp from './components/ClassComp';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  className="App">
+      <Sample/>
+      <Sample2/> 
+      <Lists/>
+      <Condition />
+      <Events/>
+      <ClassComp/>
     </div>
   );
 }
---
+++ src/components/Lists.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+function Lists() {
+    let data=[
+        {
+            name:"john",
+            email:"johnsonbaby@gmail.com",
+            age:34
+
+        },
+        {
+            name:"smith",
+            email:"smith@gmail.com",
+            age:20
+
+        },
+        {
+            name:"Ian",
+            email:"IAn@gmail.com",
+            age:35
+
+        }
+    ];
+    
+  
+  return (
+    <div>{
+       
+            
+                <div key={index}  >
+                <h1>{item.name} </h1>
+                <p>{item.email}</p>
+                <p>{item.age}</p>
+                </div>
+
+    }
+        
+        }</div>
+  )
+}
+
+export default Lists
